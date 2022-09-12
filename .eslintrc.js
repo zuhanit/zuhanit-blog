a8ac8d7fba76fa54cc4af207000ce28919b24fe8
zuhanit <zuhanit3@gmail.com>
2022-09-12 14:59:04 +0900

    fix: Disable unnecessary eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,21 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   plugins: ["react", "unused-imports"],
   rules: {
-    indent: ["error", 4],
+    indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     "unused-imports/no-unused-imports": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", "ts", "tsx"] },
+    ],
   },
 };
