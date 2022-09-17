a1980f3c421967e2a2369ff9b6f6d520b2abdf9c
zuhanit <zuhanit3@gmail.com>
2022-09-17 09:30:20 +0900

    feat: Add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,18 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="kr">
       <Head>
+        <title>Zuhanit</title>
+        <meta charSet="utf-8"></meta>
+        <meta
+          name="naver-site-verification"
+          content="2ca8f890c6c33a763d8e72430ac18dd916c0ee08"
+        />
+        <meta
+          name="google-site-verification"
+          content="x6O2ooGrTYxU6AMm-9keopUnGH4wbgA4gvoLhzrIpro"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
